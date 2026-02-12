2ad6112916ac9654338f9323e4d6d1d35cdf6091
samira-2002-code <samiraassemlali2@gmail.com>
2026-02-12 16:25:18 +0100

    functions is created

--- js/todo.js
+++ js/todo.js
@@ -1,3 +1,63 @@
+let tasks=[];
+export function initTodo(){
+  const key ="flowstate_tasks";
+  function saveTasks(){
+    localStorage.setItem(key,JSON.stringify(tasks))
+
+    
+  }
+}
+
+function loadTasks(){
+  const stored = localStorage.getItem(key);
+  if(stored){
+    tasks=JSON.parse(stored);
+  }
+}
+
+function addData(){
+  const input =document.getElementById("add");
+  if(input.value.trim()==="")return;
+   const newTask={
+    id:Data.now(),
+    task:input.value,
+    done:false,
+   };
+   tasks.push(newTask)
+   input.value="";
+   saveTasks();
+   renderTasks();
+
+}
+
+function delData(id){
+  tasks=tasks.filter((task) => task.id!==id);
+  saveTasks();
+  renderTasks();
+}
+ 
+function toggleDone(id){
+  tasks=tasks.map((task)=>
+  task.id ===id ? {...task,done :!task.done} :task);
+  saveTasks();
+  renderTasks()
+}
+
+
+function renderTasks(){
+  const list=document.querySelector(".task-list");
+  list.innerHTML="";
+  const emptyState=document.querySelector(".empty-state");
+  list.innerHTML="";
+  if(tasks.length===0){
+    emptyState.classList.remove("hidden");
+    return;
+  }else{
+   emptyState.classList.add("hidd")
+  }
+
+}
+
 
 
 
