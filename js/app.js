976a5b50c6dfff75d6a9e0326f21cb2e962db4e2
samira-2002-code <samiraassemlali2@gmail.com>
2026-02-13 15:53:25 +0100

    import of timer ui todo in app js

--- js/app.js
+++ js/app.js
@@ -1,35 +1,29 @@
-import { renderDashboard } from "./ui.js";
 
+import { renderTimer } from "./timer.js";
+import { renderDashboard } from "./ui.js";
+import { initTodo } from "./todo.js";
 
 document.addEventListener("click",(e)=>{
     if(e.target.closest("#timerBtn")){
         renderTimer();
     }
-})
+    
+  if (e.target.closest("#backBtn")) {
+    renderDashboard();
+    initTodo(); 
+  }
+});
 
 
 
-// import { renderDashboard } from "./ui.js";
-// import { renderTimer } from "./timer.js";
-// import { initTodo } from "./todo.js";
 
-// document.addEventListener("click", (e) => {
 
-//   if (e.target.closest("#timerBtn")) {
-//     renderTimer();
-//   }
 
-//   if (e.target.closest("#backBtn")) {
-//     renderDashboard();
-//     initTodo(); 
-//   }
 
-// });
 
-//   renderDashboard();
-//   initTodo();
 
-// console.log("object");
+
+
 
 
 
